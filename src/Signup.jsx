86146c8fe9709a74fb267b9a1d02e99ef7f74633
Akshay Deshmukh <114093342+AkshayBDeshmukh@users.noreply.github.com>
2024-07-01 11:43:03 +0530

    Add files via upload

---
+++ src/Signup.jsx
@@ -0,0 +1,77 @@
+import { useState} from "react"
+import { Link, Navigate } from "react-router-dom"
+import { resolve } from 'path'
+import axios from 'axios'
+import { useNavigate } from "react-router-dom"
+
+function Signup(){
+    const [name,setName]=useState()
+    const [email,setEmail]=useState()
+    const [password,setPassword]=useState()
+    const usegate=useNavigate()
+
+    const handleSubmint=(e)=>{
+        e.preventDefault()
+        axios.post('mongodb://localhost:3001/register',{name,email,password})
+        .then(result=>{console.log(result)
+         Navigate('/login')
+        })
+        .catch(err=>console.log(err)) 
+    }
+
+    return(
+        <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
+            <div className="bg-white p-3 rounded w-25">
+                <h2>Login</h2>
+                <from onSubmit={handleSubmint}>
+                <div className="mb-3">
+                    <label htmlFor="email">
+                       <strong>Name</strong>
+                    </label>
+                    <input
+                     type="text"
+                     placeholder="Enter Name"
+                     autoCapitalize="off"
+                     name="email"
+                     className="form-control rounded-0"
+                     />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email">
+                       <strong>Email</strong>
+                    </label>
+                    <input
+                     type="text"
+                     placeholder="Enter Email"
+                     autoCapitalize="off"
+                     name="email"
+                     className="form-control rounded-0"
+                     />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email">
+                       <strong>Password</strong>
+                    </label>
+                    <input
+                     type="text"
+                     placeholder="Enter Password"
+                     autoCapitalize="off"
+                     name="password"
+                     className="form-control rounded-0"
+                     />
+                </div>
+                < button type="Submit" className="btn btn-success w-100 rounded-0">
+                    Register
+                </button>
+                </from>
+                <p> Already Have an Account</p>
+                <Link to='/login'  className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">
+                    Login
+                </Link>
+                
+            </div>
+        </div>
+    );
+}
+
+export default Signup;
